accdf6df2e08430eda2e9ee62fc3601d803f6355
adarshsingh888 <singh273013@gmail.com>
2025-04-04 18:22:41 +0530

    env var name changed for backend

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_APP_URL});
+const API = axios.create({ baseURL: "https://socialapp-983a.onrender.com/social/"});
 
 export const signUP = (formData) => API.post('/public/signup', formData);
 
